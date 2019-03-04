57613246db838f27c378d1736416f5086f3cc7e9
Daniel Schneider <daschne8@gmail.com>
2019-03-04 16:35:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,6 +80,7 @@ def all_holidays_with_bbq(holiday_hash)
     holidays.each do |holiday,supplies|
       if supplies.include?("BBQ")
         bbq_holidays << holiday
+      end
     end
   end
 return bbq_holidays
